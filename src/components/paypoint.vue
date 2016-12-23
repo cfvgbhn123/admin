33d1d24bcf01d7b8939a30802f79911e993b4d6a
PC201612061110\Administrator <cfvgbhn123@126.com>
2016-12-23 18:13:33 +0800

    cc

--- src/components/paypoint.vue
+++ src/components/paypoint.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>
-        付费转化付费转化付费转化付费转化付费转化付费转化付费转化付费转化付费转化
+    <div id="pay-point">
+        <div id="pay-level">
+            <div id="pay-level-title">
+                <h3>付费等级</h3>
+            </div>
+            <el-tabs type="border-card">
+                <el-tab-pane label="付费金额">
+                  <aside>
+                      
+                  </aside>
+                </el-tab-pane>
+                <el-tab-pane label="付费次数">
+                   <aside>
+                      
+                  </aside>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
     </div>
 </template>
 <style lang="sass" scoped>
-    @import "../sass/paypoint"
+    @import "../sass/paypoint";
 </style>
